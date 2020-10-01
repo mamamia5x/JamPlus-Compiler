1be31d156209fff58e1749932a639229f5a9dfa2
Bugs Bunny <57536929+mamamia5x@users.noreply.github.com>
2020-10-01 15:22:34 -0500

    Fixed Commits

---
+++ readme.md
@@ -0,0 +1,36 @@
+## **JamPlus**
+JamPlus is a simple easy to use language, with very simple commands. It is written in HTML5, JS and CSS, without the help of Node JS or NPM. It is a sequal to Jam, which is written in Node JS, but in order to convert it over to HTML5, I would've needed to done more work than just creating a new version. The site can be found here at: https://Jam-Site.mamamia5x.repl.co
+
+
+##### *If you want to see documentation, go to the wiki found [here](https://github.com/mamamia5x/JamPlus-Compiler/wiki).*
+### Commands
+- startsay
+  - The first thing the script says once it starts.
+
+### Rules
+This language is set up different than other languages.
+- When you want to run a command, the line needs to be just the command name.
+- The next line will be the argument, if needed.
+  - For example, the first line would be "startsay", then the next line would be what you want the program to say when started.
+
+### Version History
+
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Builds Version
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 1.10.20.2 - Changed readme
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 1.10.20.1 - Put scripts and style into seperate folders.
+
+### Upcomming Updates
+#### In Progress
+  * More commands, like if statements, input, variables.
+#### Planned
+  * Documentation (but I of course need more commands).
+#### Nice to Have
+  * Syntax Highlighting
+  * Addons
+
+### Notes
+While being a compiler (more of a parser), the language is also being built onto here. Updates may take longer to happen.
+
+The Master Branch is the current running version of the project, other branches are either older versions or a new version in development.
+
+Builds Version is the changes in the build.
---
+++ script/script.js
@@ -0,0 +1,131 @@
+window.requestFileSystem  = window.requestFileSystem || window.webkitRequestFileSystem;
+readfile();
+function onInitFs(fs) {
+  var filestuff = document.getElementById('script').value;
+  fs.root.getFile('log.txt', {create: true}, function(fileEntry) {
+
+    // Create a FileWriter object for our FileEntry (log.txt).
+    fileEntry.createWriter(function(fileWriter) {
+
+      fileWriter.onwriteend = function(e) {
+        console.log('Wrote: ' + filestuff);
+      };
+
+      fileWriter.onerror = function(e) {
+        console.log('Write failed: ' + e.toString());
+      };
+      // Create a new Blob and write it to log.txt.
+      var blob = new Blob([filestuff], {type: 'text/plain'});
+
+      fileWriter.write(blob);
+
+    }, errorHandler);
+
+  }, errorHandler);
+
+}
+function remove(){
+  window.requestFileSystem(window.PERSISTANT, 1024*1024, function(fs) {
+    fs.root.getFile('log.txt', {create: false}, function(fileEntry) {
+
+      fileEntry.remove(function() {
+        console.log('File removed.');
+      }, errorHandler);
+
+    }, errorHandler);
+  }, errorHandler);
+}
+function writestuff(){
+  remove();
+  setTimeout(writeytighty,1000);
+  setTimeout(readfile,1100);
+}
+function writeytighty(){
+  window.requestFileSystem(window.PERSISTANT, 1024*1024, onInitFs, errorHandler);
+}
+function readFS(fs) {
+
+  fs.root.getFile('log.txt', {}, function(fileEntry) {
+    
+    // Get a File object representing the file,
+    // then use FileReader to read its contents.
+    fileEntry.file(function(file) {
+       var reader = new FileReader();
+
+       reader.onloadend = function(e) {
+         var txtArea = document.getElementById('script');
+         var totranslate = this.result;
+         txtArea.innerHTML = this.result;
+         translate(totranslate);
+       };
+
+       reader.readAsText(file);
+    }, errorHandler);
+
+  }, errorHandler);
+ 
+
+}
+function readfile(){
+  window.requestFileSystem(window.TEMPORARY, 1024*1024, readFS, errorHandler);
+}
+function errorHandler(e) {
+  var msg = '';
+  switch (e.code) {
+    case e.QUOTA_EXCEEDED_ERR:
+      msg = 'QUOTA_EXCEEDED_ERR';
+      break;
+    case e.NOT_FOUND_ERR:
+      msg = 'NOT_FOUND_ERR';
+      break;
+    case e.SECURITY_ERR:
+      msg = 'SECURITY_ERR';
+      break;
+    case e.INVALID_MODIFICATION_ERR:
+      msg = 'INVALID_MODIFICATION_ERR';
+      break;
+    case e.INVALID_STATE_ERR:
+      msg = 'INVALID_STATE_ERR';
+      break;
+    default:
+      msg = 'Unknown Error';
+      break;
+  };
+
+  console.log('Error: ' + msg);
+}
+
+function translate(script){
+  var lines = script.split('\n');
+  var script2 = '';
+  var switdh = window.innerWidth * .5;
+  var sheight = window.innerHeight * .4;
+  script = 'jQuery(function($, undefined) { $("#term_demo").terminal(function(command) {';
+  var secondpart = '}, { ';
+  var thirdpart = 'height: ' + sheight + ', width: ' + switdh + ', prompt: "index.jam >" });});'
+
+   var i = 0;
+  while (i < lines.length){
+    if (lines[i] == 'startsay'){
+      script2 = script2 + 'greetings: "' + lines[i + 1] + '",';
+      i++; 
+    }
+    else 
+    i++;
+  }
+  script = script + secondpart + script2 + thirdpart;
+  writelascript(script);
+}
+function writelascript (script){
+  if (srcy !== undefined){
+    srcy.remove();
+  }
+  document.getElementById("term_demo").remove();
+  var div = document.createElement("DIV");
+  div.setAttribute('id', 'term_demo');
+  document.getElementById("rightside").appendChild(div);
+  var srcy = document.createElement("SCRIPT");
+  srcy.innerHTML = script;
+  console.log(script);
+  document.body.appendChild(srcy);
+}
